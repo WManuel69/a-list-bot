da6210f7e5201afdd98d70b8b8992b01dd94d2c0
SohailSaghar <202004674@post.au.dk>
2023-01-15 18:07:44 +0100

    Update gas_price.js

--- commands/gas_price.js
+++ commands/gas_price.js
@@ -6,12 +6,11 @@ module.exports = {
   		.setName('gas_price')
   		.setDescription('See the current gas prices'),
 	async execute(interaction) {
-		let embed = new EmbedBuilder()
-		.setColor(0x0099FF)
-		.setTitle('Gas price')
-		.setThumbnail('https://cdn.discordapp.com/attachments/1059490759994249267/1062655683557855342/JPG-04.jpg')
 		const resp = axios.get("https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=NQVJFVIWT2Q83ZZ2MCYYI54G1ZWY5TJT9X")
-			.then((response) => {embed
+			.then((response) => {const embed = new EmbedBuilder()
+				.setColor(0x0099FF)
+				.setTitle('Gas price')
+				.setThumbnail('https://cdn.discordapp.com/attachments/1059490759994249267/1062655683557855342/JPG-04.jpg')
 				.addFields(
 					{ name: 'Slow', value: response.data.result.SafeGasPrice, inline: true },
 					{ name: 'Normal', value: response.data.result.ProposeGasPrice, inline: true },

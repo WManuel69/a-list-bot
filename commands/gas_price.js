24fc3d48ddab6cc2e203f68a63ecbdbf6214a6de
SohailSaghar <202004674@post.au.dk>
2023-01-15 16:28:20 +0100

    trying with fetch

--- commands/gas_price.js
+++ commands/gas_price.js
@@ -5,18 +5,19 @@ module.exports = {
   		.setName('gas_price')
   		.setDescription('See the current gas prices'),
 	async execute(interaction) {
-		const data = fetch("https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=NQVJFVIWT2Q83ZZ2MCYYI54G1ZWY5TJT9X").then((response) => response.json());
-		const json = JSON.parse(data);
-		const embed = new EmbedBuilder()
-			.setColor(0x0099FF)
-			.setTitle('Gas price')
-			.setThumbnail('https://cdn.discordapp.com/attachments/1059490759994249267/1062655683557855342/JPG-04.jpg')
-			.addFields(
-				{ name: 'Slow', value: json.result.SafeGasPrice, inline: true },
-				{ name: 'Medium', value: json.result.ProposeGasPrice, inline: true },
-				{ name: 'Fast', value: json.result.FastGasPrice, inline: true },
-			);
-
-		await interaction.send({ embeds: [embed] });
+		const data = fetch("https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=NQVJFVIWT2Q83ZZ2MCYYI54G1ZWY5TJT9X")
+			.then((response) => response.json())
+			.then((d) => {const embed = new EmbedBuilder()
+				.setColor(0x0099FF)
+				.setTitle('Gas price')
+				.setThumbnail('https://cdn.discordapp.com/attachments/1059490759994249267/1062655683557855342/JPG-04.jpg')
+				.addFields(
+					{ name: 'Slow', value: d.result.SafeGasPrice, inline: true },
+					{ name: 'Medium', value: d.result.ProposeGasPrice, inline: true },
+					{ name: 'Fast', value: d.result.FastGasPrice, inline: true },
+				);
+	
+			interaction.reply({ embeds: [embed] });} );
+		
 	},
 }

7c096fafae1886b9937e301d5e4ff488c289e42f
Manuel <80989598+0xManuel@users.noreply.github.com>
2023-01-15 21:34:36 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -6,57 +6,51 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('floor_price')
 		.setDescription('See the floor price for any project.')
-		.addStringOption(option => 
+		.addStringOption(option =>
 			option.setName('input')
 				.setDescription('Contract address or URL name')),
 	async execute(interaction) {
 		const input = interaction.options.get("input").value;
-		let address = "";
 		if (input.startsWith("0x") && input.length == 42) {
-			address = input;
+			const settings = {
+				apiKey: "tpZ8EEIC8zHtYWd8xQ5gChmVK7vb2jiE", // Replace with your Alchemy API Key.
+				network: Network.ETH_MAINNET, // Replace with your network.
+			};
+
+			const alchemy = new Alchemy(settings);
+
+			alchemy.nft.getFloorPrice(input).then((d) => {
+				const embed = new EmbedBuilder()
+					.setColor(0x0099FF)
+					.setTitle('Floor price')
+					.setThumbnail('https://cdn.discordapp.com/attachments/1059490759994249267/1062655683557855342/JPG-04.jpg')
+					.addFields(
+						{ name: 'Floor Price:', value: `${d.openSea.floorPrice}Ξ` }
+					);
+				interaction.reply({ embeds: [embed] });
+			});
 		} else {
 			try {
 				axios.get(`https://api.opensea.io/api/v1/collection/${input}`)
-				.then((res) => {
-					console.log(res.data.collection.stats.floor_price);
-					const embed = new EmbedBuilder()
-						.setColor(0x0099FF)
-						.setTitle('Floor price')
-						.setThumbnail('https://cdn.discordapp.com/attachments/1059490759994249267/1062655683557855342/JPG-04.jpg')
-						.addFields(
-							{ name: 'Floor Price:', value: ` ETH` } // res.data.collection.stats.floor_price
-						);
-					interaction.reply({ embeds: [embed] });
-					return;
-				}); 
+					.then((res) => {
+						console.log(res.data.collection.stats.floor_price);
+						const embed = new EmbedBuilder()
+							.setColor(0x0099FF)
+							.setTitle('Floor price')
+							.setThumbnail('https://cdn.discordapp.com/attachments/1059490759994249267/1062655683557855342/JPG-04.jpg')
+							.addFields(
+								{ name: 'Floor Price:', value: `${res.data.collection.stats.floor_price}Ξ` } 
+							);
+						interaction.reply({ embeds: [embed] });
+						return;
+					});
 			} catch (error) {
 				await interaction.reply("Collection does not exist. Try again, otherwise use contract address")
 				return;
 			}
-			
-			
+
+
 		}
-
-		const settings = {
-			apiKey: "tpZ8EEIC8zHtYWd8xQ5gChmVK7vb2jiE", // Replace with your Alchemy API Key.
-			network: Network.ETH_MAINNET, // Replace with your network.
-		};
-
-		const alchemy = new Alchemy(settings);
-
-		alchemy.nft.getFloorPrice(address).then((d) => {
-			const embed = new EmbedBuilder()
-			.setColor(0x0099FF)
-			.setTitle('Floor price')
-			.setThumbnail('https://cdn.discordapp.com/attachments/1059490759994249267/1062655683557855342/JPG-04.jpg')
-			.addFields(
-				{ name: 'Floor Price:', value: "hi" } //d.openSea.floorPrice
-			);
-		interaction.reply({ embeds: [embed] });
-		});
-		
-	
-		
 	},
 };
 

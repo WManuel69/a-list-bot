11ec76599457e9ace29c61fba2a5ff4f88efbeab
SohailSaghar <202004674@post.au.dk>
2023-01-15 13:44:24 +0100

    Update account.js

--- commands/account.js
+++ commands/account.js
@@ -1,14 +1,33 @@
-const { Client, Collection, Events, GatewayIntentBits, REST, Routes, SlashCommandBuilder } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits, REST, Routes, SlashCommandBuilder, channelLink } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
   		.setName('account')
-  		.setDescription('See the floor price for any project.')
+  		.setDescription('Check ETH wallet balance')
   		.addStringOption(option => 
-    		option.setName('test')
-      	.setDescription('Contract address or URL name')),
+    		option.setName('input')
+      	.setDescription('Wallet address')),
 	async execute(interaction) {
-    const input = interaction.options.getString('test');
-		await interaction.reply(input);
+    const input = interaction.options.getString('input');
+		const address = interaction.options.get("input").value;
+		if (address.length != 42) {
+			await interaction.reply("Wrong address given")
+			return
+		}
+
+		const { Alchemy, Utils } = require('alchemy-sdk');
+
+		const apiKey = "tpZ8EEIC8zHtYWd8xQ5gChmVK7vb2jiE";
+		const settings = {
+    		apiKey: apiKey
+		};
+
+		const alchemy = new Alchemy(settings);
+
+		// Get balance and format in terms of ETH
+		let balance = await alchemy.core.getBalance(address, 'latest');
+		balance = Utils.formatEther(balance);
+		await interaction.reply(`Balance of ${address}: ${balance} ETH`);
+	
 	},
 }

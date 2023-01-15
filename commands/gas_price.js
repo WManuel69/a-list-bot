6e0e6461ae881c17bbc1a38b283fa833feb6af7d
SohailSaghar <202004674@post.au.dk>
2023-01-15 17:35:37 +0100

    Update gas_price.js

--- commands/gas_price.js
+++ commands/gas_price.js
@@ -1,23 +1,24 @@
 const { Client, Collection, Events, GatewayIntentBits, REST, Routes, SlashCommandBuilder, channelLink, EmbedBuilder } = require('discord.js');
+const { default: axios } = require("axios");
 
 module.exports = {
     data: new SlashCommandBuilder()
   		.setName('gas_price')
   		.setDescription('See the current gas prices'),
 	async execute(interaction) {
-		const data = fetch("https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=NQVJFVIWT2Q83ZZ2MCYYI54G1ZWY5TJT9X")
-			.then((response) => response.json())
-			.then((d) => {const embed = new EmbedBuilder()
+		axios.get("https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=NQVJFVIWT2Q83ZZ2MCYYI54G1ZWY5TJT9X")
+			.then((response) => {const embed = new EmbedBuilder()
 				.setColor(0x0099FF)
 				.setTitle('Gas price')
 				.setThumbnail('https://cdn.discordapp.com/attachments/1059490759994249267/1062655683557855342/JPG-04.jpg')
 				.addFields(
-					{ name: 'Slow', value: d.result.SafeGasPrice, inline: true },
-					{ name: 'Normal', value: d.result.ProposeGasPrice, inline: true },
-					{ name: 'Fast', value: d.result.FastGasPrice, inline: true },
+					{ name: 'Slow', value: response.result.data.SafeGasPrice, inline: true },
+					{ name: 'Normal', value: response.result.data.ProposeGasPrice, inline: true },
+					{ name: 'Fast', value: response.data.result.FastGasPrice, inline: true },
 				);
 	
-			interaction.reply({ embeds: [embed] });} );
+			interaction.reply({ embeds: [embed] });})
 		
-	},
+		
+	}
 }

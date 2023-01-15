c550890c7dea35736fa7b51c14b0af98fe6c51a1
SohailSaghar <202004674@post.au.dk>
2023-01-15 12:36:43 +0100

    Update floorprice.js

--- commands/floorprice.js
+++ commands/floorprice.js
@@ -2,13 +2,14 @@ const { Client, Collection, Events, GatewayIntentBits, REST, Routes, SlashComman
 
 module.exports = {
 	data: new SlashCommandBuilder()
-  .setName('test')
-  .setDescription('See the floor price for any project.')
-  .addStringOption(option => 
-    option.setName('test')
-      .setDescription('Contract address or URL name')),
+  		.setName('test')
+  		.setDescription('See the floor price for any project.')
+  		.addStringOption(option => 
+    		option.setName('test')
+      	.setDescription('Contract address or URL name')),
 	async execute(interaction) {
-		await interaction.reply("Hi");
+    const input = interaction.options.getString('test');
+		await interaction.reply(input);
 	},
 };
   

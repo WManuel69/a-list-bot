ea72947d7859dcf1cc387ed4488f952bbe29d1cc
SohailSaghar <202004674@post.au.dk>
2023-01-16 19:35:52 +0100

    Update project_trait.js

--- commands/project_trait.js
+++ commands/project_trait.js
@@ -1,14 +1,22 @@
-const { Client, Collection, Events, GatewayIntentBits, REST, Routes, SlashCommandBuilder } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits, REST, Routes, SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { default: axios } = require("axios");
+const { Network, Alchemy } = require("alchemy-sdk");
 
 module.exports = {
     data: new SlashCommandBuilder()
   		.setName('project_trait')
   		.setDescription('See the floor price for any project.')
   		.addStringOption(option => 
-    		option.setName('test')
+    		option.setName('input')
       	.setDescription('Contract address or URL name')),
 	async execute(interaction) {
-    const input = interaction.options.getString('test');
-		await interaction.reply(input);
+		const input = interaction.options.get('input').value;
+		const embed = new EmbedBuilder()
+			.setColor(0x0099FF)
+			.setTitle('Balance of you')
+			.setDescription(`$ ETH`)
+			.setThumbnail('https://cdn.discordapp.com/attachments/1059490759994249267/1062655683557855342/JPG-04.jpg');
+		let updated = EmbedBuilder.from(embed.embeds[0]);
+		await interaction.reply(updated); 
 	},
 };

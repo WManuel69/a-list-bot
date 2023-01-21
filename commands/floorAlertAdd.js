62d76ac1da34aedcf441dad604458ac69d8b7e98
SohailSaghar <202004674@post.au.dk>
2023-01-21 18:23:04 +0100

    Update floorAlertAdd.js

--- commands/floorAlertAdd.js
+++ commands/floorAlertAdd.js
@@ -22,27 +22,84 @@ module.exports = {
 		const contractAddress = interaction.options.get('address').value;
 		const increment = interaction.options.get('increment').value;
 		const dbName = "Alist"
-		try {
-            await client.connect();
-            console.log("Connected correctly to server");
-            const db = client.db(dbName);
-            // Use the collection "people"
-            const col = db.collection("contractAddresses");
-            // Construct a document                                                                                                                                                              
-            let personDocument = {
-                "contractAddress": contractAddress,
-                "increment": increment
-            }
-            await col.insertOne(personDocument);
-            
-           } catch (err) {
-            await interaction.reply("error occurred")
-        }
-    
-        finally {
-           await client.close();
-           await interaction.reply("Collection added!")
+        const user = interaction.user.id;
+        if (input.startsWith("0x") && input.length == 42) {
+			const settings = {
+				apiKey: "tpZ8EEIC8zHtYWd8xQ5gChmVK7vb2jiE", // Replace with your Alchemy API Key.
+				network: Network.ETH_MAINNET, // Replace with your network.
+			};
 
-       }
+			const alchemy = new Alchemy(settings);
+            // Just to check if the collection does exist 
+			alchemy.nft.getFloorPrice(contractAddress).then((d) => {
+				let collectionName = d.openSea.collectionUrl.substr(d.openSea.collectionUrl.indexOf("collection/")+11,d.openSea.collectionUrl.length);
+				axios.get(`https://api.opensea.io/api/v1/collection/${collectionName}`).then((resp) => {
+					try {
+                        client.connect();
+                        console.log("Connected correctly to server");
+                        const db = client.db(dbName);
+                        // Use the collection "people"
+                        const col = db.collection("contractAddresses");
+                        // Construct a document                                                                                                                                                              
+                        let personDocument = {
+                            "contractAddress": contractAddress,
+                            "increment": increment,
+                            "userID": user
+                        }
+                        col.insertOne(personDocument);
+                        
+                    } catch (err) {
+                        interaction.reply("error occurred")
+                    }
+                
+                    finally {
+                       client.close();
+                       interaction.reply("Collection added!")
+                   }
+
+
+					interaction.reply({ embeds: [embed] });}).catch((err) => interaction.reply("Wrong contract address"));
+			}).catch((err) => interaction.reply("Wrong contract address"));
+		} else {
+			try {
+				axios.get(`https://api.opensea.io/api/v1/collection/${contractAddress}`)
+					.then((res) => {
+						try {
+                            client.connect();
+                            console.log("Connected correctly to server");
+                            const db = client.db(dbName);
+                            // Use the collection "people"
+                            const col = db.collection("contractAddresses");
+                            // Construct a document                                                                                                                                                              
+                            let personDocument = {
+                                "contractAddress": res.data.collection.primary_asset_contracts[0].address,
+                                "increment": increment,
+                                "userID": user
+                            }
+                            col.insertOne(personDocument);
+                            
+                        } catch (err) {
+                            interaction.reply("error occurred")
+                        }
+                    
+                        finally {
+                           client.close();
+                           interaction.reply("Collection added!")
+                
+                       }
+					
+						
+					})
+					.catch((err) => {
+						interaction.reply(
+							"Collection does not exist. Try again, otherwise use contract address");
+					});
+			} catch (error) {
+				await interaction.reply("Collection does not exist. Try again, otherwise use contract address")
+				return;
+			}
+
+
+		}
 	},
 };

9083789c2cc33fc0fa827563d7aed3de4e759661
SohailSaghar <202004674@post.au.dk>
2023-01-29 12:51:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -62,10 +62,13 @@ setInterval(async () => {
         db.collections().then(async (col) => {
             for (let i = 0; i < stats.collections; i++) {
                 let collect = db.collection(col[i].s.namespace.collection);
-                if (collect.countDocuments() == 0) {
+                if (collect.countDocuments().then(p => {
+                    if (p == 0) {
                     db.removeCollection(col[i].s.namespace.collection);
                     return console.log("No documents in collection");
-                }
+                    }
+                }));
+
                 const entries = collect.find({});
                 const oneElement = collect.findOne({});
                 if (!ethers.utils.isAddress(col[i].s.namespace.collection)) return console.log("Not a contract address")

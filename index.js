2bef1f6fed58e8d90436fafdb46b9b1d2c290ad1
Manuel <80989598+0xManuel@users.noreply.github.com>
2023-01-29 11:27:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-return 
 const { Client, Collection, Events, GatewayIntentBits, REST, Routes, SlashCommandBuilder, channelLink, EmbedBuilder } = require('discord.js');
 const { default: axios } = require("axios");
 const { Network, Alchemy } = require("alchemy-sdk");
